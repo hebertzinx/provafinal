bac40dceb97810411e4b2aece6c8d3c531912ab2
HEBERT GABRIEL DOS ANJOS LOPES <232.976393@universidadesenaicimatec.edu.br>
2026-03-31 16:14:17 -0300

    Fix: Adicionando chaves para conexao direta

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'CHAVE_SERA_CONFIGURADA_NA_VERCEL'
-const supabaseKey = ''
+const supabaseUrl = 'lhihjyjyuphtfbsluqdr'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoaWhqeWp5dXBodGZic2x1cWRyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDk3NTEzNCwiZXhwIjoyMDkwNTUxMTM0fQ.VcQOOSf5tb-hl6VomJWdHKfEHk5uezK9fypfzyF2iME'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function salvarPaciente(event) {
